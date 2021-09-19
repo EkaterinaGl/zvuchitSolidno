6229b020eaf68c0f53574a380f61091519b6e914
Glushkova Ekaterina <37184010+EkaterinaGl@users.noreply.github.com>
2021-09-19 09:02:47 +0300

    Результат Хакатона
    К сожалению, наша разработка на данный момент утеряна

---
+++ ITGazprom/ITGazprom/ITGazprom.py
@@ -0,0 +1,59 @@
+
+#скважина по номеру выводится на общий экран
+def well():
+	return well
+
+#стволы привязаны к скважинам и выводятся автоматически при указании определённой скважины
+def trunk():
+	return trunk
+
+#аналогично стволу
+def direction():
+	return direction
+
+
+def true_bearing():
+	true_bearing = 32
+	return true_bearing
+
+def magnetic_azimuth():
+	magnetic_azimuth = 23
+	return magnetic_azimuth
+
+def directional_angel():
+	directional_angel = 34
+	return directional_angel
+
+def retreat():
+	retreat = 43
+	return retreat
+
+def shift():
+	shift = 34
+	return shift
+
+def Am_VTC():
+	Am_VTC = 323
+	return Am_VTC
+
+def A_VTC():
+	A_VTC = 324
+	return A_VTC
+
+def magnetic_declination():
+	magneticDecline = 33
+	return magneticDecline
+
+def convergence_of_meridians():
+	convergence_of_meridianss = 23
+	return convergence_of_meridians
+
+#для расчёта маркшейдера нужна формула
+def surveyor():
+	return
+
+#данные скважины (номер скважины, широта и долгота). такие данные находятся в бд, отвечающей за общую информацию по скважинам
+def borehole_coordinates():
+	well_number = ?
+	northernLatitude = ?
+	easternLongitude = ?
